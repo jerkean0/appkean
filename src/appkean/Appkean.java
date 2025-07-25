e2327ee658521da7c465ef51798838fb0764c3e4
jerkean0 <jerkeangabrina39@gmail.com>
2025-07-25 00:19:19 -0700

    first push on github

---
+++ src/appkean/Appkean.java
@@ -0,0 +1,23 @@
+
+package appkean;
+
+import java.util.Scanner;
+
+
+public class Appkean {
+
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String name;
+        int byear;
+        
+        System.out.print("Enter your name: ");
+        name = sc.nextLine();
+        System.out.print("Enter your birth year; ");
+        byear = sc.nextInt();
+        
+       System.out.println("Hello "+name+" your age is "+ (2025 - byear));
+    }
+    
+}
---
+++ src/appkean/activity2.java
@@ -0,0 +1,11 @@
+
+package appkean;
+
+
+public class activity2 {
+    
+    public static void main(String[] args) {
+        System.out.println("hello activity 2 ");
+    }
+   
+}
